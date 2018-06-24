f18992003cbff561df72c14678baffa41f30aa28
Jose Bujalance <joseanbu23@gmail.com>
2018-06-24 12:14:31 +0200

    Add Proportional min to max strategy

--- src/main/java/com/bujalance/betting/Main.java
+++ src/main/java/com/bujalance/betting/Main.java
@@ -2,10 +2,7 @@ package com.bujalance.betting;
 
 import com.bujalance.betting.model.Bookmaker;
 import com.bujalance.betting.parser.BettingEventProvider;
-import com.bujalance.betting.strategy.Gambler;
-import com.bujalance.betting.strategy.MaximumQuoteStrategy;
-import com.bujalance.betting.strategy.MiddleQuoteStrategy;
-import com.bujalance.betting.strategy.MinimumQuoteStrategy;
+import com.bujalance.betting.strategy.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,16 +13,18 @@ public class Main {
 	private final static Logger fLogger = LogManager.getLogger(Main.class);
 
 	public static void main(String[] args) throws IOException {
-		double betPerMatch = 2;
-		Gambler minimumQuoteGambler = new Gambler(new MinimumQuoteStrategy(betPerMatch));
-		Gambler maximumQuoteGambler = new Gambler(new MaximumQuoteStrategy(betPerMatch));
-		Gambler middleQuoteGambler = new Gambler(new MiddleQuoteStrategy(betPerMatch));
+		double baseQuantity = 2;
+		Gambler minimumQuoteGambler = new Gambler(new MinimumQuoteStrategy(baseQuantity));
+		Gambler maximumQuoteGambler = new Gambler(new MaximumQuoteStrategy(baseQuantity));
+		Gambler middleQuoteGambler = new Gambler(new MiddleQuoteStrategy(baseQuantity));
+		Gambler proportionalMinToMaxGambler = new Gambler(new ProportionalMinToMaxQuoteStrategy(baseQuantity));
 
 		Bookmaker bookmaker = Bookmaker.WILLIAM_HILL;
-		fLogger.info("Start betting € {} per match on {}", betPerMatch, bookmaker.name());
+		fLogger.info("Start betting € {} as base quantity on {}", baseQuantity, bookmaker.name());
 		fLogger.info("Minimum quote strategy: € " + minimumQuoteGambler.betOnEvents(new BettingEventProvider(bookmaker)));
 		fLogger.info("Maximum quote strategy: € " + maximumQuoteGambler.betOnEvents(new BettingEventProvider(bookmaker)));
 		fLogger.info("Middle quote strategy: € " + middleQuoteGambler.betOnEvents(new BettingEventProvider(bookmaker)));
+		fLogger.info("Proportional min to max quote strategy: € " + proportionalMinToMaxGambler.betOnEvents(new BettingEventProvider(bookmaker)));
 		fLogger.info("End");
 	}
 
---
+++ src/main/java/com/bujalance/betting/strategy/ProportionalMinToMaxQuoteStrategy.java
@@ -0,0 +1,26 @@
+package com.bujalance.betting.strategy;
+
+import com.bujalance.betting.model.BettingEvent;
+import com.bujalance.betting.model.Odd;
+
+/**
+ * We bet on the minimum quote odd with a variable quantity.
+ * The quantity to bet is given by the formula: {@code quantity = baseQuantity * maxQuote / minQuote}.
+ * Thus, the bigger gap between odds, the bigger quantity is bet, reducing the loses.
+ */
+public class ProportionalMinToMaxQuoteStrategy extends AbstractStrategy {
+
+	public ProportionalMinToMaxQuoteStrategy(final double pBaseQuantity) {
+		super(pBaseQuantity);
+	}
+
+	@Override
+	public Odd getChosenOdd(final BettingEvent pEvent) {
+		return pEvent.getMinimumQuoteOdd();
+	}
+
+	@Override
+	public double getQuantity(final BettingEvent pEvent) {
+		return fBaseQuantity * (pEvent.getMaximumQuoteOdd().getQuote() / pEvent.getMinimumQuoteOdd().getQuote());
+	}
+}
